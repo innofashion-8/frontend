16220df47ec02771547eb25b2144fea960e0be3c
clarenceevanw <clarenceevan0907@gmail.com>
2026-03-19 12:06:07 +0700

    build for deploy

---
+++ .next/server/pages/api/complete-registration/submit.js
@@ -0,0 +1,5 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/pages/api/complete-registration/submit.js")
+R.c("server/chunks/[root-of-the-server]__9873d2b4._.js")
+R.c("server/chunks/[root-of-the-server]__ec476382._.js")
+R.m(61005)
+module.exports=R.m(61005).exports
