394cbdfc79155feb4fd39aea85618986a0a6df42
clarenceevanw <clarenceevan0907@gmail.com>
2026-03-19 11:57:28 +0700

    feat: add palette hardcode

---
+++ src/config/palette.ts
@@ -0,0 +1,12 @@
+const palette = {
+  onyx: "#1C1C1B",
+  obsidian: "#1a1a1a",
+  walnut: "#6A5D52",
+  greige: "#B7AC9B",
+  ash: "#979086",
+  stucco: "#E2E2DE",
+  graphite: "#494947",
+  gravel: "#7b787a",
+};
+
+export default palette;
