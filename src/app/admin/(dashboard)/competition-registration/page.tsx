cd698714784beccc140a1cb68bda8113764955af
clarenceevanw <clarenceevan0907@gmail.com>
2026-04-23 17:54:49 +0700

    refactor: search and add button export

--- src/app/admin/(dashboard)/competition-registration/page.tsx
+++ src/app/admin/(dashboard)/competition-registration/page.tsx
@@ -1,11 +1,18 @@
 import RegistrationClient from "@/components/admin/competition-registration/RegistrationClient";
 import { registrationService } from "@/services/registration-service";
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ page?: string }> }) {
+export default async function Page({ searchParams }: { searchParams: Promise<{ page?: string; search?: string; status?: string; competition_name?: string; category?: string; user_type?: string }> }) {
   const params = await searchParams;
   const page = parseInt(params.page || "1");
+  const search = params.search || "";
+  const status = params.status || "";
+  const competition_name = params.competition_name || "";
+  const category = params.category || "";
+  const user_type = params.user_type || "";
   
-  const response = await registrationService.getCompetitionsRegistrations(page);
+  const response = await registrationService.getCompetitionsRegistrations({
+    page, search, status, competition_name, category, user_type
+  });
 
   return (
     <RegistrationClient 
--- src/app/admin/(dashboard)/event-registration/page.tsx
+++ src/app/admin/(dashboard)/event-registration/page.tsx
@@ -1,11 +1,17 @@
 import RegistrationClient from "@/components/admin/event-registration/RegistrationClient";
 import { registrationService } from "@/services/registration-service"; // 👈 Import yang SERVER
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ page?: string }> }) {
+export default async function Page({ searchParams }: { searchParams: Promise<{ page?: string; search?: string; status?: string; event_name?: string; user_type?: string }> }) {
   const params = await searchParams;
   const page = parseInt(params.page || "1");
+  const search = params.search || "";
+  const status = params.status || "";
+  const event_name = params.event_name || "";
+  const user_type = params.user_type || "";
   
-  const response = await registrationService.getEventRegistrations(page);
+  const response = await registrationService.getEventRegistrations({
+    page, search, status, event_name, user_type
+  });
 
   return (
     <RegistrationClient 
