2abd5e3bbd710abb869f779ee1c68bba33a35af8
clarenceevanw <clarenceevan0907@gmail.com>
2026-05-13 15:28:46 +0700

    refactor: paginated response

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,10 +3,12 @@ export interface PaginatedResponse<T> {
   current_page: number;
   data: T[]; 
   first_page_url: string;
+  from: number;
   last_page: number;
   last_page_url: string;
   next_page_url: string | null;
-  prev_page_url: string | null;
   per_page: number;
+  prev_page_url: string | null;
+  to: number;
   total: number;
 }
