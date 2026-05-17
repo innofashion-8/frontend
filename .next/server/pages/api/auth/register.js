36daf95bbc40da1cf0c7c0619d1021a89efde9bf
clarenceevanw <clarenceevan0907@gmail.com>
2026-05-17 22:37:36 +0700

    remove build

--- .next/server/pages/api/auth/register.js
+++
@@ -1,5 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/pages/api/auth/register.js")
-R.c("server/chunks/[root-of-the-server]__81eb7e1e._.js")
-R.c("server/chunks/[root-of-the-server]__ec476382._.js")
-R.m(71039)
-module.exports=R.m(71039).exports
