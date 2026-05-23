5609585f55ea93e3bd33e3437ba02f9751923896
clarenceevanw <clarenceevan0907@gmail.com>
2026-05-23 17:21:23 +0700

    remove build

--- .next/server/pages/api/admin/divisions/[id].js
+++
@@ -1,5 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/pages/api/admin/divisions/[id].js")
-R.c("server/chunks/[root-of-the-server]__a744fa91._.js")
-R.c("server/chunks/[root-of-the-server]__ec476382._.js")
-R.m(16121)
-module.exports=R.m(16121).exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9oaT87oZ4FSprySJSssXACL5EGDz7FI19EPnpAG87LQ=\"\n}"
