2ef32a67334efd843c72949a726531f8f77fad06
clarenceevanw <clarenceevan0907@gmail.com>
2026-05-23 18:09:44 +0700

    remove build

--- .next/server/pages/api/admin/roles-permissions/roles.js
+++
@@ -1,5 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/pages/api/admin/roles-permissions/roles.js")
-R.c("server/chunks/[root-of-the-server]__5fbb6acf._.js")
-R.c("server/chunks/[root-of-the-server]__ec476382._.js")
-R.m(20224)
-module.exports=R.m(20224).exports
